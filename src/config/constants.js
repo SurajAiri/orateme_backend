5bcdb9ca10ac46bed6d3595b32d71d23c2af570c
SurajAiri <surajairi.ml@gmail.com>
2025-02-04 23:22:33 +0530

    railway-deploy

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,14 @@
-export const DEFAULT_PAGE = 1;
-export const DEFAULT_LIMIT = 15;
+const DEFAULT_PAGE = 1;
+const DEFAULT_LIMIT = 15;
 
-export const AUDIO_BUCKET = 'audios-orateme';
-export const VIDEO_BUCKET = 'videos-orateme';
-export const PROFILE_BUCKET = 'profiles-orateme';
+const AUDIO_BUCKET = 'audios-orateme';
+const VIDEO_BUCKET = 'videos-orateme';
+const PROFILE_BUCKET = 'profiles-orateme';
+
+export {
+    DEFAULT_PAGE,
+    DEFAULT_LIMIT,
+    AUDIO_BUCKET,
+    VIDEO_BUCKET,
+    PROFILE_BUCKET
+};
